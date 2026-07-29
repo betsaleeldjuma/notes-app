b763ddc314fcaee57827431f9282d6c29ce2ea33
betsaleeldjuma <djumabestaleel@gmail.com>
2026-07-29 16:45:22 +0200

    Fix the DB

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async (DATABASE_URL) => {
     try {
-        await mongoose.connect(DATABASE_URL)
-        console.log('Data base connected...')
+        const conn = await mongoose.connect(DATABASE_URL)
+        console.log(`Data base connected... Connected: ${conn.connection.host}`)
     } catch(error) {
         console.log(error)
     }
